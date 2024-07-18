c1e6b383777d7866c99cf2d1f1c6d5fb104e8e2a
IamAugustine <40532366+IamAugustine@users.noreply.github.com>
2024-07-17 21:05:30 -0400

    initial commit

---
+++ Sing_In_Your_Voice/Sing In Your Voice/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sing_In_Your_Voice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
